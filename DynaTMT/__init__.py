2f2eeec1f69318718e02474aab32ccfe46fc34a0
Süleyman Bozkurt <sbozkurt.mbg@gmail.com>
2026-03-19 13:48:42 +0100

    v2.9.4 - Update module metadata in __init__.py

--- DynaTMT/__init__.py
+++ DynaTMT/__init__.py
@@ -1,8 +1,8 @@
-# __author__ = 'Kevin Klann'
-# __updated_by__ = 'Süleyman Bozkurt'
-# __version__ = '2.9.3'
-# __updated_date__ = '2026-03-18'
-# __description__ = 'DynaTMT - updates and fixes.'
-# __license__ = 'MIT'
-# __init__.py
+__author__ = "Kevin Klann - Süleyman Bozkurt"
+__version__ = "2.9.4"
+__maintainer__ = "Süleyman Bozkurt"
+__email__ = "sbozkurt.mbg@gmail.com"
+__date__ = '18.01.2021'
+__update__ = '19.03.2026'
+
 from .DynaTMT import PD_input, plain_text_input
